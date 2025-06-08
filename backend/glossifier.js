2e271b1cbb6b0a2a7b41b0166068b7d1ff5f7458
yourname <yourname@gmail.com>
2025-06-08 13:58:25 +0300

    day1 implemented uploading file and changing it to gloss removing unsigned words and filetered out the signed words

---
+++ backend/glossifier.js
@@ -0,0 +1,27 @@
+// glossifier.js
+
+// Optional dictionary to normalize words
+const glossDictionary = {
+  reading: "READ",
+  reads: "READ",
+  student: "STUDENT",
+  book: "BOOK",
+  biology: "BIOLOGY",
+  is: "", // remove
+  the: "", // remove
+  a: "",
+};
+
+const stopWords = new Set(["is", "the", "a", "an", "in", "on", "of", "about", "are", "was"]);
+
+function glossify(text) {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s]/g, "") // remove punctuation
+    .split(/\s+/)            // tokenize
+    .filter(word => !stopWords.has(word))
+    .map(word => glossDictionary[word] || word.toUpperCase())
+    .filter(Boolean); // remove empty strings
+}
+
+module.exports = { glossify };
