251892107ab5981f442551e405a13d11721393dd
yourname <adamsvillage326@gmail.com>
2025-10-24 22:47:01 +0300

    adding a credentials in env

--- Ethslproj/backend/server.js
+++ Ethslproj/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path   = require('path');
 const app   = express();
-const port= process.env.port || 3000;
+const port= process.env.PORT || 3000;
 const routes = require('./routes');
 
 app.use(cors());
